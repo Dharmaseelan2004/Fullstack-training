76bbbe1eee2e5d4a5984568c5f8aa2f87f6b2c9e
Dharmaseelan2004 <dharmaseelan12112004@gmail.com>
2024-12-30 21:31:11 +0530

    eightcommit

---
+++ secondDatatype.js
@@ -0,0 +1,31 @@
+//array = [10,20,30,40,[50.6],"a","kec",true]
+//console.log(array);
+
+//object
+
+/*obj = {
+    "firstname":"Dharma",
+    "lastname":"seelan",
+    "age":20
+}
+console.log(obj);*/
+
+//objtype2 = {}
+//console.log(objtype2);
+
+
+objtype3 = new Object()
+objtype3["phone"]="samsung"
+objtype3["store"]="playstore"
+objtype3["game1"]="pubg"
+objtype3["game2"]="freefire"
+console.log(objtype3);
+console.log(objtype3["game2"]);
+
+objtype2 = {}
+objtype2["movie"] = "batman"
+objtype2 ["budget"]= "100 cr"
+objtype2 ["boxoffice"]= "200 cr"
+objtype2 ["release"]= "worldwide"
+console.log(objtype2)
+console.log(objtype2["boxoffice"]);
